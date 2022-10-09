37e7cdfaa2af3c4ea1d93bfe37e6b1acbdc6a870
Dominik <245842@.edu.p.lodz.pl>
2022-10-09 17:11:01 +0200

    Zmiany dodatkowe w 03

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/student/CLionProjects/untitled
+# Install script for directory: D:/dominik/programy/oprogramowanie_zuytkowe_c/cwiczenie_1/cwiczenie_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/student/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/dominik/programy/oprogramowanie_zuytkowe_c/cwiczenie_1/cwiczenie_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
