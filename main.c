86b7b8f5e58a0fb07688e7ff551bfa059bf0baa6
Dominik <245842@.edu.p.lodz.pl>
2022-10-09 17:24:18 +0200

    Zmiana w 03 (5)

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int policz(int a , int b){
+    return (a+b)*(a-b);
     return a*b;
 }
 
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu ; (2+3)*(2-3) = %d", policz(2,3));
     printf("W pierwszym branchu ; 2*3 = %d", policz(2,3));
     //komentarz
     // praca domowa dokończ i na wikamp wgrać link do repo
